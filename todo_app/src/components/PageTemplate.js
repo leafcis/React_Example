db604f48d76ea1d7b3dca87a9aa43dac6965e38b
unknown <leaf_cis@naver.com>
2018-09-06 23:50:52 +0900

    TODO앱

---
+++ todo_app/src/components/PageTemplate.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styles from './PageTemplate.scss';
+import classNames from 'classnames/bind'
+
+class PageTemplate extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default PageTemplate;
